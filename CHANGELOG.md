2cf696f0d30e4db928fe6e0d8c47798d632d03f8
NerdSnipe <nerdsnipe@users.noreply.github.com>
2023-04-05 17:03:58 +0700

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `laravel-countries` will be documented in this file.
+
+## Initial Release - 2023-04-03
+
+Our initial release. Please see the README for details on the package.
---
+++ config/laravel-countries.php
@@ -0,0 +1,21 @@
+<?php
+
+// config for NerdSnipe/LaravelCountries
+
+return [
+
+    /**
+     * The default country to select if none set
+     */
+    'default_country' => 'US',
+
+    /**
+     * Where should the component place the JavaScript within your view
+     */
+    'script_stack' => 'post-app-scripts',
+
+    /**
+     * Where should the component place the CSS styles within your view
+     */
+    'style_stack' => 'post-app-css',
+];
